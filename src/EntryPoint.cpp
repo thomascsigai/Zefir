97641c1ce67224c7b95b1af6c4e81d0cd46b7b3d
Thomas Csigai <thomascsigai1@gmail.com>
2025-01-16 17:21:43 +0100

    Added main loop + Event processing

--- src/EntryPoint.cpp
+++ src/EntryPoint.cpp
@@ -7,13 +7,14 @@
 const std::uint16_t SCREEN_WIDTH = 800;
 const std::uint16_t SCREEN_HEIGHT = 600;
 
-int main(int argc, char* argv[])
-{
-	// The window we'll be rendering to
-	SDL_Window* window = NULL;
+// The window we'll be rendering to
+SDL_Window* gWindow = NULL;
+// The surface contained by the window
+SDL_Surface* gScreenSurface = NULL;
 
-	// The surface contained by the window
-	SDL_Surface* screenSurface = NULL;
+bool init()
+{
+	bool success = true;
 
 	// SDL Initialization
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
@@ -23,32 +24,80 @@ int main(int argc, char* argv[])
 	else
 	{
 		//Create Window
-		window = SDL_CreateWindow("GameDev QuickStart", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		
-		if (window == NULL)
+		gWindow = SDL_CreateWindow("GameDev QuickStart", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+		if (gWindow == NULL)
 		{
 			std::cerr << "Window could not be created ! SDL_Error: " << SDL_GetError() << std::endl;
+			success = false;
 		}
 		else
 		{
 			// Get window surface
-			screenSurface = SDL_GetWindowSurface(window);
-
-			///Fill the surface white
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-
-			//Update the surface
-			SDL_UpdateWindowSurface(window);
-
-			//Hack to get window to stay up
-			SDL_Event e; bool quit = false; while (quit == false) { while (SDL_PollEvent(&e)) { if (e.type == SDL_QUIT) quit = true; } }
+			gScreenSurface = SDL_GetWindowSurface(gWindow);
 		}
 	}
-	//Destroy window
-	SDL_DestroyWindow(window);
 
-	//Quit SDL subsystems
+	return success;
+}
+
+void close()
+{
+	// Deallocate surface
+	SDL_FreeSurface(gScreenSurface);
+
+	//Destroy Window
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+
 	SDL_Quit();
+}
+
+int main(int argc, char* argv[])
+{
+	bool quit = false;
+
+	SDL_Event e;
+
+	init();
+
+	while (!quit)
+	{
+		while (SDL_PollEvent(&e) != 0)
+		{
+			if (e.type == SDL_QUIT)
+			{
+				quit = true;
+			}
+			else if (e.type == SDL_KEYDOWN)
+			{
+				switch (e.key.keysym.sym)
+				{
+					case SDLK_UP:
+					std::cout << "UP pressed" << std::endl;
+					break;
+					
+					case SDLK_DOWN:
+					std::cout << "DOWN pressed" << std::endl;
+					break;
+					
+					case SDLK_RIGHT:
+					std::cout << "RIGHT pressed" << std::endl;
+					break;
+					
+					case SDLK_LEFT:
+					std::cout << "LEFT pressed" << std::endl;
+					break;
+
+					default:
+					std::cout << "A key is pressed" << std::endl;
+					break;
+				}
+			}
+		}
+	}
+
+	close();
 
 	return 0;
 }
