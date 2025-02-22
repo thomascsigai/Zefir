2ab66eaeb959e46da4b65053273991a2a82d7324
thomascsigai <thomascsigai1@gmail.com>
2025-02-22 16:01:39 +0100

    Refacto Texture files

--- Djipi/core/resource/Texture.h
+++ Djipi/core/resource/Texture.h
@@ -13,60 +13,13 @@ namespace Djipi
 	class Texture : public Resource
 	{
 	public:
-		Texture(const std::string& path, Renderer* renderer)
-		{
-			m_Path = path;
-			m_Renderer = renderer;
-			m_SDLTexture = nullptr;
-		}
+		Texture(const std::string& path, Renderer* renderer);
 
-		bool Load() override
-		{ 
-			if (m_SDLTexture != nullptr)
-			{
-				Unload();
-			}
+		bool Load() override;
+		bool Unload() override;
+		bool Reload() override;
 
-			if (m_Renderer == nullptr)
-			{
-				LOG_WARN("Renderer could not be null while loading texture.");
-				return false;
-			}
-
-			SDL_Surface* surface = IMG_Load(m_Path.c_str());
-			if (surface == nullptr)
-			{
-				LOG_WARN("Surface for texture " << m_Path << " not created." << SDL_GetError());
-				return false;
-			}
-
-			m_SDLTexture = SDL_CreateTextureFromSurface(m_Renderer->GetSDLRenderer(), surface);
-			SDL_FreeSurface(surface);
-			
-			if (m_SDLTexture == nullptr)
-			{
-				LOG_WARN("Texture " << m_Path << " could not be loaded." << SDL_GetError());
-				return false;
-			}
-
-			return true;
-		}
-
-		bool Unload() override
-		{
-			SDL_DestroyTexture(m_SDLTexture);
-			return true;
-		}
-
-		bool Reload() override
-		{
-			return Load();
-		}
-
-		SDL_Texture* GetSDLTexture() const
-		{ 
-			return m_SDLTexture; 
-		}
+		SDL_Texture* GetSDLTexture() const;
 
 	private:
 		Renderer* m_Renderer;
