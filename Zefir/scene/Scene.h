0f533257ec8f4848ba26af7a1daf85e36b199d9d
Thomas Csigai <thomascsigai1@gmail.com>
2025-05-19 17:38:41 +0200

    Update scene interface

--- Zefir/scene/Scene.h
+++ Zefir/scene/Scene.h
@@ -1,15 +1,22 @@
 #pragma once
 
+#include <core/EngineContext.h>
+
 namespace Zefir
 {
 	class Scene
 	{
+	protected:
+		EngineContext* m_EngineContext;
+
 	public:
 		virtual ~Scene() = default;
 		virtual void OnLoad() {}
 		virtual void OnUnload() {}
 		virtual void Update(double deltaTime) = 0;
-		virtual void Render() = 0;
+		virtual void Render(const std::unique_ptr<Renderer>& renderer) = 0;
 		virtual void OnEvent(const SDL_Event& e) {}
+
+		virtual void OnAttach(EngineContext* context) { m_EngineContext = context; }
 	};
 }
