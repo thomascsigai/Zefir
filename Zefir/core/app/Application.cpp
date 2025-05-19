2e54fb53516dee53b4e3c1b710d9bb88cac69240
Thomas Csigai <thomascsigai1@gmail.com>
2025-05-19 15:57:27 +0200

    Updating Application Class

---
+++ Zefir/core/app/Application.cpp
@@ -0,0 +1,76 @@
+#include <core/app/Application.h>
+
+namespace Zefir
+{
+	Application::Application() : m_IsRunning(false)
+	{
+		m_DeltaTime = 0;
+	}
+
+	bool Application::Init()
+	{
+		if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0)
+		{
+			LOG_FATAL("SDL could not initialize ! SDL_Error: " << SDL_GetError());
+			return false;
+		}
+
+		// Systems instanciation & initialization
+		m_Window = std::make_unique<Window>();
+		m_Renderer = std::make_unique<Renderer>(m_Window.get());
+		m_ResourceManager = std::make_unique<ResourceManager>(m_Renderer.get());
+		m_SoundManager = std::make_unique<SoundManager>();
+		m_SceneManager = std::make_unique<SceneManager>();
+
+		// Setup EngineContext
+		m_EngineContext.window = m_Window.get();
+		m_EngineContext.renderer = m_Renderer.get();
+		m_EngineContext.resourceManager = m_ResourceManager.get();
+		m_EngineContext.soundManager = m_SoundManager.get();
+
+		OnInit();
+
+		m_IsRunning = true;
+		return true;
+	}
+
+	void Application::Update()
+	{
+		m_SceneManager->Update(m_DeltaTime);
+	}
+
+	void Application::OnEvent(SDL_Event& e)
+	{
+		while (SDL_PollEvent(&e) != 0)
+		{
+			if (e.type == SDL_QUIT)
+			{
+				m_IsRunning = false;
+			}
+
+			m_SceneManager->OnEvent(e);
+			CustomEvents(e);
+		}
+	}
+
+	void Application::Render()
+	{
+		SDL_SetRenderDrawColor(m_Renderer->GetSDLRenderer(), 0, 0, 0, 255);
+		SDL_RenderClear(m_Renderer->GetSDLRenderer());
+		SDL_SetRenderDrawColor(m_Renderer->GetSDLRenderer(), 255, 255, 255, 255);
+
+		m_SceneManager->Render(m_Renderer);
+
+		SDL_RenderPresent(m_Renderer->GetSDLRenderer());
+	}
+
+	void Application::Exit()
+	{
+		SDL_Quit();
+	}
+
+	bool Application::IsRunning()
+	{
+		return m_IsRunning;
+	}
+}
--- Zefir/core/app/Application.h
+++ Zefir/core/app/Application.h
@@ -3,6 +3,7 @@
 #include <SDL.h>
 
 #include <core/window/Window.h>
+#include <core/EngineContext.h>
 #include <renderer/Renderer.h>
 #include <sound/SoundManager.h>
 #include <resource/ResourceManager.h>
@@ -12,7 +13,7 @@ namespace Zefir
 {
 	class Application
 	{
-	private:
+	protected:
 		// Application systems
 		std::unique_ptr<Window> m_Window;
 		std::unique_ptr<Renderer> m_Renderer;
@@ -20,73 +21,23 @@ namespace Zefir
 		std::unique_ptr<SoundManager> m_SoundManager;
 		std::unique_ptr<SceneManager> m_SceneManager;
 
+		EngineContext m_EngineContext;
+
 	public:
-		Application() : m_IsRunning(false)
-		{}
+		Application();
 
-		bool Init()
-		{
-			if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0)
-			{
-				LOG_FATAL("SDL could not initialize ! SDL_Error: " << SDL_GetError());
-				return false;
-			}
+		bool Init();
+		virtual void OnInit() = 0;
+		
+		void Update();
+		
+		void OnEvent(SDL_Event& e);
+		virtual void CustomEvents(SDL_Event& e) = 0;
+		
+		void Render();
+		void Exit();
 
-			// Systems instanciation & initialization
-			m_Window = std::make_unique<Window>();
-			m_Renderer = std::make_unique<Renderer>(m_Window.get());
-			m_ResourceManager = std::make_unique<ResourceManager>(m_Renderer.get());
-			m_SoundManager = std::make_unique<SoundManager>();
-			m_SceneManager = std::make_unique<SceneManager>();
-
-			m_IsRunning = true;
-			return true;
-		}
-
-		void Update()
-		{
-			m_SceneManager->Update(m_DeltaTime);
-		}
-
-		void OnEvent(SDL_Event& e)
-		{
-			while (SDL_PollEvent(&e) != 0)
-			{
-				if (e.type == SDL_QUIT)
-				{
-					m_IsRunning = false;
-				}
-
-				m_SceneManager->OnEvent(e);
-			}
-		}
-
-		void Render()
-		{
-			SDL_SetRenderDrawColor(m_Renderer->GetSDLRenderer(), 0, 0, 0, 255);
-			SDL_RenderClear(m_Renderer->GetSDLRenderer());
-			SDL_SetRenderDrawColor(m_Renderer->GetSDLRenderer(), 255, 255, 255, 255);
-
-			m_SceneManager->Render();
-
-			SDL_RenderPresent(m_Renderer->GetSDLRenderer());
-		}
-
-		void Exit()
-		{
-			SDL_Quit();
-		}
-
-		bool IsRunning() 
-		{
-			return m_IsRunning;
-		}
-
-		SDL_Renderer* GetSDLRenderer()
-		{
-			return m_Renderer->GetSDLRenderer();
-		}
-
+		bool IsRunning();
 		double m_DeltaTime;
 
 	private:
