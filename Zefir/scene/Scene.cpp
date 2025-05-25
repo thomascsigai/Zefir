d330ae507c3e784b7fb3cace7788c796f2194427
Thomas Csigai <thomascsigai1@gmail.com>
2025-05-25 20:47:36 +0200

    Adding a scene cpp file

---
+++ Zefir/scene/Scene.cpp
@@ -0,0 +1,45 @@
+#include <scene/Scene.h>
+
+namespace Zefir
+{
+	Scene::Scene()
+	{
+		m_EngineContext = nullptr;
+		m_SceneObjects.reserve(10);
+		m_PhysicsWorld = std::make_unique<PhysicsWorld>();
+	}
+
+	void Scene::OnEvent(const SDL_Event& e)
+	{
+		for (std::unique_ptr<GameObject>& go : m_SceneObjects)
+		{
+			go->HandleEvent(e);
+		}
+		OnSceneEvent(e);
+	}
+
+	void Scene::Update(double deltaTime)
+	{
+		for (std::unique_ptr<GameObject>& go : m_SceneObjects)
+		{
+			go->Update(deltaTime);
+		}
+		m_PhysicsWorld->Step(deltaTime);
+	}
+
+	void Scene::Render(const std::unique_ptr<Renderer>& renderer)
+	{
+		for (std::unique_ptr<GameObject>& go : m_SceneObjects)
+		{
+			go->Render(renderer);
+		}
+	}
+
+	void Scene::OnAttach(EngineContext* context) { m_EngineContext = context; }
+
+	void Scene::AddObjectToScene(std::unique_ptr<GameObject> go)
+	{
+		m_SceneObjects.push_back(std::move(go));
+		m_PhysicsWorld->AddObject(m_SceneObjects.back().get());
+	}
+}
--- Zefir/scene/Scene.h
+++ Zefir/scene/Scene.h
@@ -13,51 +13,20 @@ namespace Zefir
 		std::vector<std::unique_ptr<GameObject>> m_SceneObjects;
 		std::unique_ptr<PhysicsWorld> m_PhysicsWorld;
 
-		void AddObjectToScene(std::unique_ptr<GameObject> go)
-		{
-			m_SceneObjects.push_back(std::move(go));
-			m_PhysicsWorld->AddObject(m_SceneObjects.back().get());
-		}
+		void AddObjectToScene(std::unique_ptr<GameObject> go);
 
 	public:
-		Scene()
-		{
-			m_EngineContext = nullptr;
-			m_SceneObjects.reserve(10);
-			m_PhysicsWorld = std::make_unique<PhysicsWorld>();
-		}
-
+		Scene();
 		virtual ~Scene() = default;
+
 		virtual void OnLoad() {}
 		virtual void OnUnload() {}
+		// Events handled by the user in his scene
 		virtual void OnSceneEvent(const SDL_Event& e) = 0;
 
-		void OnEvent(const SDL_Event& e)
-		{
-			for (std::unique_ptr<GameObject>& go : m_SceneObjects)
-			{
-				go->HandleEvent(e);
-			}
-			OnSceneEvent(e);
-		}
-
-		void Update(double deltaTime)
-		{
-			for (std::unique_ptr<GameObject>& go : m_SceneObjects)
-			{
-				go->Update(deltaTime);
-			}
-			m_PhysicsWorld->Step(deltaTime);
-		}
-
-		void Render(const std::unique_ptr<Renderer>& renderer)
-		{
-			for (std::unique_ptr<GameObject>& go : m_SceneObjects)
-			{
-				go->Render(renderer);
-			}
-		}
-
-		void OnAttach(EngineContext* context) { m_EngineContext = context; }
+		void OnEvent(const SDL_Event& e);
+		void Update(double deltaTime);
+		void Render(const std::unique_ptr<Renderer>& renderer);
+		void OnAttach(EngineContext* context);
 	};
 }
