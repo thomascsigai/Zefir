5f0fadc0a8e5185be579379a9b5e296e9ea17e70
Thomas Csigai <thomascsigai1@gmail.com>
2025-05-25 20:38:39 +0200

    Added a plot to show fps

--- Zefir/core/debug/zefir-debug-ui/PerformanceFrame.h
+++ Zefir/core/debug/zefir-debug-ui/PerformanceFrame.h
@@ -6,6 +6,8 @@ namespace Zefir
 {
 	struct PerformanceFrame : public ImGuiFrame
 	{
+		
+
 		std::string GetName() override
 		{
 			return ("Performance");
@@ -13,7 +15,19 @@ namespace Zefir
 
 		void ShowContent() override
 		{
-			ImGui::Text("FPS: %.1f", ImGui::GetIO().Framerate);
+			static float fpsHistory[100] = { 0.0f };
+			static int fpsIndex = 0;
+						
+			fpsHistory[fpsIndex] = ImGui::GetIO().Framerate;
+			fpsIndex = (fpsIndex + 1) % IM_ARRAYSIZE(fpsHistory);
+
+			float avg = 0.0f;
+			for (float f : fpsHistory) avg += f;
+			avg /= IM_ARRAYSIZE(fpsHistory);
+
+			ImGui::Text("FPS: %.1f", fpsHistory[(fpsIndex - 1 + IM_ARRAYSIZE(fpsHistory)) % IM_ARRAYSIZE(fpsHistory)]);
+			ImGui::PlotLines("FPS", fpsHistory, IM_ARRAYSIZE(fpsHistory), fpsIndex,
+				"FPS", 0.0f, 144.0f, ImVec2(0, 80));
 			ImGui::Text("Delta Time: %.3f ms", 1000.0f / ImGui::GetIO().Framerate);
 		}
 	};
