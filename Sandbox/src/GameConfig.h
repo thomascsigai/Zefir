21c9e9826277cda037c8212bd99856697f80a064
Thomas Csigai <thomascsigai1@gmail.com>
2025-05-27 16:20:26 +0200

    Moving gameconfig inside apps

---
+++ Sandbox/src/GameConfig.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <core/window/WindowSettings.h>
+
+namespace ZefirApp
+{
+	// Paddles keybinds
+	struct MoveKeybind
+	{
+		SDL_Keycode UP;
+		SDL_Keycode DOWN;
+		SDL_Keycode LEFT;
+		SDL_Keycode RIGHT;
+	};
+
+	constexpr MoveKeybind MOVE_KEYBIND = { SDLK_UP, SDLK_DOWN, SDLK_LEFT, SDLK_RIGHT };
+
+	constexpr int PLAYER_SPEED = 1000;
+}
