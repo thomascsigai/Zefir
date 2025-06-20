9702a1a4f0edc78ee8d83bfa059142d61e328b46
Thomas Csigai <thomascsigai1@gmail.com>
2025-06-20 17:19:41 +0200

    Adding operator overloads between Vector2 and b2Vec2

--- Zefir/core/math/Vector2.cpp
+++ Zefir/core/math/Vector2.cpp
@@ -34,6 +34,18 @@ namespace Zefir
 		x -= v.x; y -= v.y;
 		return (*this);
 	}
+	
+	Vector2& Vector2::operator +=(const b2Vec2& v)
+	{
+		x += v.x, y += v.y;
+		return (*this);
+	}
+
+	Vector2& Vector2::operator -=(const b2Vec2& v)
+	{
+		x -= v.x; y -= v.y;
+		return (*this);
+	}
 
 	std::ostream& operator<<(std::ostream& os, const Vector2& v)
 	{
